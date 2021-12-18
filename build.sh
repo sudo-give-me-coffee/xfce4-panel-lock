b2648bb9e8cdaacc91cd3a79496f3324a71358a2
Natanael <natanaelgplay@gmail.com>
2021-12-18 11:23:59 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ apt-get source xfce4-panel
 
 cd xfce4-panel-*
 
-linha1=$(grep -n "^static GtkMenu \*$" xfce-panel-plugin.c  | cut -d\: -f1)
+linha1=$(grep -n "^static GtkMenu \*$" libxfce4panel/xfce-panel-plugin.c  | cut -d\: -f1)
 
-linha2=$(grep -n "return plugin->priv->menu;" xfce-panel-plugin.c  | cut -d\: -f1)
+linha2=$(grep -n "return plugin->priv->menu;" libxfce4panel/xfce-panel-plugin.c  | cut -d\: -f1)
 
 
 sed -i "${linha1},${linha2}s/gtk_widget_show (item);/\/\/ gtk_widget_show (item);/" libxfce4panel/xfce-panel-plugin.c
