045f1ef8c576fddeb9b4b077f81a56bbcd8d92e5
Natanael <natanaelgplay@gmail.com>
2021-12-18 12:08:57 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,11 +11,14 @@ apt-get source xfce4-panel
 cd xfce4-panel-*
 
 linha1=$(grep -n "^static GtkMenu \*$" libxfce4panel/xfce-panel-plugin.c  | cut -d\: -f1)
-
 linha2=$(grep -n "return plugin->priv->menu;" libxfce4panel/xfce-panel-plugin.c  | cut -d\: -f1)
-
-
 sed -i "${linha1},${linha2}s/gtk_widget_show (item);/\/\/ gtk_widget_show (item);/" libxfce4panel/xfce-panel-plugin.c
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+linha1=$(grep -n "^xfce_panel_plugin_button_press_event (GtkWidget      \*widget,$" xfce-panel-plugin.c  | cut -d\: -f1)
+linha2=$(($(grep -n "gtk_menu_popup_at_pointer (menu, (GdkEvent \*) event);$" xfce-panel-plugin.c  | cut -d\: -f1)+1))
+sed -i "${linha1},${linha2}s/gtk_menu_popup_at_pointer (menu, (GdkEvent/\/\/ gtk_menu_popup_at_pointer (menu, (GdkEvent/" libxfce4panel/xfce-panel-plugin.c
 
 
 dpkg-buildpackage -b
